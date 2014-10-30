995832aecb9a2952bf13844a8b8b671d36ff6b18
saasbook <saasbook@saasbook.(none)>
2014-10-30 00:17:42 -0300

    first featureC

--- picker.rb
+++ picker.rb
@@ -1,9 +1,22 @@
-name=""
-
 def pick_engineer()
 	engineer_name = IO.readlines("engineers.txt")
-	random = rand(engineer_name.size) 
+	random = rand(engineer_name.size)
 	return engineer_name[random]
 end
-puts pick_engineer()
 
+def featureC(search)
+	random_name = pick_engineer() 
+	if search.length >= 3 and search.length < random_name.length
+		 boo_name = random_name.include?(search)
+		if  boo_name == true
+			puts "1. " + random_name
+		else
+			puts " Search not have "
+		end
+			
+	else
+		puts "Search the name with any minimum 3 character!! " 
+	end
+end
+
+featureC("ana")
