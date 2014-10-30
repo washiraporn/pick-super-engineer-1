b3850b9310585dd73c1366c70f9ddc6cf9558d5e
saasbook <saasbook@saasbook.(none)>
2014-10-29 23:53:03 -0300

    featureA

--- picker.rb
+++ picker.rb
@@ -1,9 +1,10 @@
 name=""
-
 def pick_engineer()
-	engineer_name = IO.readlines("engineers.txt")
+  engineer_name = IO.readlines("engineers.txt")
 	random = rand(engineer_name.size) 
 	return engineer_name[random]
 end
 puts pick_engineer()
-
+def pick_featureA
+  name_A=name.gsub(" ","."t)
+end
