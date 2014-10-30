46296a18d7739b48ff704f26328ca511d7296430
chenchira <chenchira123477@gmail.com>
2014-10-30 00:21:06 -0300

    add FeatureB

--- picker.rb
+++ picker.rb
@@ -1,9 +1,20 @@
 name=""
 
 def pick_engineer()
-	engineer_name = IO.readlines("engineers.txt")
-	random = rand(engineer_name.size) 
-	return engineer_name[random]
+  engineer_name = IO.readlines("engineers.txt")
+  random = rand(engineer_name.size)
+  return engineer_name[random]
+end
+def random_name()
+  content = File.readlines 'engineers.txt'
+  rand_name = content[rand(content.size)]
+  rn = rand_name.split(" ")
+  if rn.size > 1
+    new = rn[1]
+    return rn[0]+" "+new[0]+"..."
+  else
+    return rand_name
+  end
 end
 puts pick_engineer()
 
